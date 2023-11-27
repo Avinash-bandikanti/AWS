3cc053180da4456fca6750aa478b058ef2e25fe3
Avinash-bandikanti <124676800+Avinash-bandikanti@users.noreply.github.com>
2023-11-27 18:19:50 +0530

    Create xmlfile1.py

---
+++ Xml_to_json/xmlfile1.py
@@ -0,0 +1,13 @@
+<employeeinfo>
+        <employee1>
+                <name>sindhu</name>
+                <sapid>52138700</sapid>
+                <salary>200000</salary>
+        </employee1>
+        <employee2>
+                <name>gayathri</name>
+                <sapid>52138220</sapid>
+                <salary>300000</salary>
+        </employee2>
+
+</employeeinfo>
